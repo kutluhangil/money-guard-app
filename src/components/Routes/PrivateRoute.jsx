993c3638de610dd6d5d0e6a7201ecbb2277baa96
hatice_v_daalen <HaticevanD@users.noreply.github.com>
2026-02-22 14:29:26 +0100

    Route issues solved

--- src/components/Routes/PrivateRoute.jsx
+++ src/components/Routes/PrivateRoute.jsx
@@ -3,10 +3,10 @@ import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
-  // const isLoggedIn = useSelector((state) => state.auth?.isLoggedIn);
-  // if (!isLoggedIn) {
-  // return <Navigate to="/login" replace />;
-  // }
+  const isLoggedIn = useSelector((state) => state.auth?.isLoggedIn);
+  if (!isLoggedIn) {
+    return <Navigate to="/login" replace />;
+  }
   return children;
 };
 
